fa99891a3d4d8a73917aa638fe3bdccaa3f7746b
shbrezman <shbrezman@gmail.com>
2021-06-06 22:29:55 +0300

    only-ui-1

--- src/app/user-board/user-board.component.ts
+++ src/app/user-board/user-board.component.ts
@@ -31,9 +31,14 @@ export type ChartOptions = {
 export class UserBoardComponent implements OnInit {
 
   @ViewChild("chart") chart: ChartComponent;
+  @ViewChild("chart2") chart2: ChartComponent;
   public chartOptions: Partial<ChartOptions>;
+  public chartOptions2: Partial<ChartOptions>;
 
   singleScore: number;
+  practiceResults = [];
+  testsResults = [];
+
 
   demoUser = {
     name: 'shlomi brezman123',
@@ -51,33 +56,51 @@ export class UserBoardComponent implements OnInit {
     department: 'heart',
     residancyYear: 3,
     practices: [
-      { title: 'practice1', score: 88 },
-      { title: 'practice2', score: 97 },
-      { title: 'practice3', score: null },
-      { title: 'practice4', score: null },
-      { title: 'practice5', score: null },
+      { title: 'practice1', date: new Date(2020,3,6), score: 88 },
+      { title: 'practice2', date: new Date(2020,4,8), score: 97 },
+      { title: 'practice3', date: new Date(2020,6,12), score: 92 },
+      { title: 'practice4', date: null, score: null },
+      { title: 'practice5', date: null, score: null },
     ],
     tests: [
-      { title: 'test1', score: 90 },
-      { title: 'test2', score: 95 },
-      { title: 'test3', score: 65 },
-      { title: 'test4', score: null },
-      { title: 'test5', score: null },
+      { title: 'test1', date: new Date(2020,8,6), score: 90 },
+      { title: 'test2', date: new Date(2020,10,8), score: 95 },
+      { title: 'test3', date: new Date(2020,10,10), score: 89 },
+      { title: 'test4', date: null, score: null },
+      { title: 'test5', date: null, score: null },
     ]
   };
 
+  lastTest = this.demoUser.tests[0];
+  lastPractice = this.demoUser.practices[0];
+
   constructor(public userService: UserService) {
-    
+
+    this.demoUser.tests.find( test =>{
+      this.testsResults.push(test.score);
+      if(test.date > this.lastTest.date){
+        this.lastTest= test;
+      }
+    })
+
+    this.demoUser.practices.find( practice =>{
+      this.practiceResults.push(practice.score);
+      if(practice.date > this.lastPractice.date){
+        this.lastPractice = practice;
+      }
+    })
+
     this.chartOptions = {
       series: [
         {
           name: "score",
-          data: [10, 41, 35, 51, 49, 62, 69, 91]
+          data: this.testsResults,
+          color: "#66ff66"
         }
       ],
       chart: {
-        height: 280,
-        width: 350,
+        height: 210,
+        width: 430,
         type: "line",
         zoom: {
           enabled: false
@@ -89,9 +112,48 @@ export class UserBoardComponent implements OnInit {
       stroke: {
         curve: "straight"
       },
-      title: {
-        text: this.demoUser.name + " grid:",
-        align: "left"
+      grid: {
+        row: {
+          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5
+        }
+      },
+      xaxis: {
+        categories: [
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          ""
+        ]
+      },
+    };
+
+    this.chartOptions2 = {
+      series: [
+        {
+          name: "score",
+          data: this.practiceResults,
+          color: "#80ffff"
+        }
+      ],
+      chart: {
+        height: 210,
+        width: 430,
+        type: "line",
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: "straight"
       },
       grid: {
         row: {
