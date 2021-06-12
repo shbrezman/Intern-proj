f159d07b41b03c60531f58bbde2bdd45cd4244c6
shbrezman <shbrezman@gmail.com>
2021-06-13 00:51:40 +0300

    only-ui5

--- src/app/exam-page/exam-page.component.ts
+++ src/app/exam-page/exam-page.component.ts
@@ -11,18 +11,36 @@ import { UserService } from '../services/user.service';
 export class ExamPageComponent implements OnInit {
 
   charNum = 65;
+  test;
+  testSent = false;
 
   constructor(public userService: UserService, public testService: TestService, private router: Router) {
 
+    this.test = {
+      title: "math",
+      questions: [
+        {body : "2+2", answers : ["2", "4", "6", "22"], rightAnswer: "B", correct: null, examineeAnswer: ""},
+        {body : "4+2", answers : ["2", "8", "6", "62"], rightAnswer: "C", correct: null, examineeAnswer: ""},
+        {body : "11+11", answers : ["2", "42", "4", "22"], rightAnswer: "D", correct: null, examineeAnswer: ""},
+      ],
+      score: null
 
+    }
 
   }
 
-  clic(value){
-    console.log(value);
+  clic(value, qIndex){
+    this.test.questions[qIndex].examineeAnswer = String.fromCharCode(value);
+    this.test.questions[qIndex].correct = this.test.questions[qIndex].examineeAnswer == this.test.questions[qIndex].rightAnswer
+  }
 
-    console.log();
-
+  sendTest(){
+    this.testSent = true;
+    this.test.score = 0;
+    this.test.questions.forEach(element => {
+      if(element.correct) this.test.score += 10;
+    });
+    console.log(this.test.score);
 
   }
 
